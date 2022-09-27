c1f906cb7167646edcfb4f65408c2d57034c4e61
thisIsJooS <thisis.joos@gmail.com>
2022-09-27 17:40:20 +0900

    init setting

---
+++ src/main/java/ITerview/iterview/IterviewApplication.java
@@ -0,0 +1,13 @@
+package ITerview.iterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ITerview/iterview/IterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package ITerview.iterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
