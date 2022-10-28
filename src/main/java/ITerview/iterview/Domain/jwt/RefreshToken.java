80ab9d79bcfbf5a5737bfcacc804773a81c9d155
thisIsJooS <thisis.joos@gmail.com>
2022-10-29 02:13:39 +0900

    aws rds connect

--- src/main/java/ITerview/iterview/Domain/jwt/RefreshToken.java
+++ src/main/java/ITerview/iterview/Domain/jwt/RefreshToken.java
@@ -4,10 +4,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
@@ -15,9 +12,14 @@ import javax.persistence.Table;
 @Entity
 public class RefreshToken {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="refresh_token_id")
+    private Long id;
+
+    @Column(name="rt_key")
     private String key;
 
-    @Column(nullable = false)
+    @Column(name = "rt_value")
     private String value;
 
     public void updateValue(String token){
